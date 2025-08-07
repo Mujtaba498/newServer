54d82d0506400c98d7a1b46c390b6d2bbf00e3ea
Mujtaba498 <cryptoanalysis45@gmail.com>
2025-08-08 01:30:02 +0500

    Update server.js

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/health', (req, res) => {
   res.status(200).json({
     success: true,
-    message: 'GetFork Backend is running successfully!',
+    message: 'GetFork Backend is running successfully!, hahahha',
     timestamp: new Date().toISOString(),
     environment: process.env.NODE_ENV || 'development'
   });
